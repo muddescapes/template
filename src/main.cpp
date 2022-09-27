0a2a18f05ce76b5c8cff53909b9214c05c0a3e19
Ashley Cheung <ashley@Ashleys-MacBook-Air-3.local>
2022-09-27 09:56:09 -0700

    added comments to template file

--- src/main.cpp
+++ src/main.cpp
@@ -1,95 +1,33 @@
-// This ESP32 takes care of the servos controlling the door to the escape pod. 
-
 #include <Arduino.h>
 #include <ESPHelper.h>
 #include <mqtt_client.h>
-#include <ESP32Servo.h>
 
+// Connect to Claremont-ETC wifi
 constexpr const char *WIFI_NAME = "Claremont-ETC";
-constexpr const char *WIFI_PASSWORD = "cityoftrees87";
+constexpr const char *WIFI_PASSWORD = "Cl@remontI0T";
+
+// Connect to MQTT
 constexpr const char *MQTT_BROKER = "mqtt://mqtt.eclipseprojects.io";
-constexpr const char *MQTT_TOPIC = "muddescapes-esp/varmon";
-constexpr const char *MQTT_TOPIC_MANUAL = "muddescapes-esp/manual";
+constexpr const char *MQTT_TOPIC = "muddescapes-esp/test";
 
-Servo servo1; // "navigation"
-Servo servo2; // "pressurize"
-Servo servo3; // "power"
+// Declare variables: 
+// These will be set by input voltage to ESP:
+bool led_status = false;
+bool button_status = false;
 
-bool servo1_status = false;
-bool servo2_status = false;
-bool servo3_status = false;
+// This is an internal variable for me to keep track of what should be happening
+bool turn_on_led = false;
 
-// keeps track of whether or not these buttons have ever been pressed
-bool button1 = false;
-bool button2 = false;
-bool button3 = false;
+// This is an internal variable allowing me to create a "latch" from a button
+bool pressed = false;
 
-const int locked_angle = 0;
-const int unlocked_angle = 90;
-
-void lock_servos() {
-    servo1.write(locked_angle);
-    servo2.write(locked_angle);
-    servo3.write(locked_angle);
-
-    servo1_status = false;
-    servo2_status = false;
-    servo3_status = false;
-}
-
-void unlock_servos() {
-    servo1.write(unlocked_angle);
-    servo2.write(unlocked_angle);
-    servo3.write(unlocked_angle);
-
-    servo1_status = true;
-    servo2_status = true;
-    servo3_status = true;
-}
-
-void lock_servo(int servo_num) {
-    if (servo_num == 1) {
-        servo1.write(locked_angle);
-        servo1_status = false;
-    }
-    if (servo_num == 2) {
-        servo2.write(locked_angle);
-        servo2_status = false;
-    }
-    if (servo_num == 3) {
-        servo3.write(locked_angle);
-        servo3_status = false;
-    }
-}
-
-void unlock_servo(int servo_num) {
-    if (servo_num == 1) {
-        servo1.write(unlocked_angle);
-        servo1_status = true;
-    }
-    if (servo_num == 2) {
-        servo2.write(unlocked_angle);
-        servo2_status = true;
-    }
-    if (servo_num == 3) {
-        servo3.write(unlocked_angle);
-        servo3_status = true;
-    }
-}
-
-// resets button variables, not servos. use lock_servos to reset those
-void reset() {
-    button1 = false;
-    button2 = false;
-    button3 = false;
-}
+// BEGIN TEMPLATE CODE (DO NOT CHANGE)
 
 typedef void (*mqtt_callback_t)(esp_mqtt_event_handle_t);
 
 void mqtt_cb_connected(esp_mqtt_event_handle_t event) {
     Serial.println("connected to MQTT broker");
     esp_mqtt_client_subscribe(event->client, MQTT_TOPIC, 0);
-    esp_mqtt_client_subscribe(event->client, MQTT_TOPIC_MANUAL, 0);
 }
 
 static void mqtt_cb_subscribed(esp_mqtt_event_handle_t event) {
@@ -106,59 +44,30 @@ static void mqtt_cb_message(esp_mqtt_event_handle_t event) {
         return;
     }
     Serial.printf("message arrived on topic %.*s: %.*s\n", event->topic_len, event->topic, event->data_len, event->data);
+    
+    // END TEMPLATE CODE
 
-    if (strncmp(event->data, "buttcheek", event->data_len) == 0) {
-        Serial.printf("don't you mean asscheek");
-        esp_mqtt_client_publish(event->client, MQTT_TOPIC, "ill squeeze that", 0, 0, 0);
-        digitalWrite(GPIO_NUM_27,HIGH);
-    }
+    // Here, we "listen" for certain messages and can act upon them. 
+    // Example below: We listen for the string "test". If we hear it, we print to serial monitor "test message received" (internal, local)
+    // and we publish the same message to MQTT (external, public)
 
-    if (strncmp(event->data, "nut", event->data_len) == 0) {
-        Serial.printf("don't you mean cashew");
-        esp_mqtt_client_publish(event->client, MQTT_TOPIC, "ill milk that", 0, 0, 0);
-        digitalWrite(GPIO_NUM_27,LOW);
-    }
-
-    if (strncmp(event->data, "unlock servos override", event->data_len) == 0) {
-        esp_mqtt_client_publish(event->client, MQTT_TOPIC_MANUAL, "[Control Panel] Overriding servos - UNLOCK", 0, 0, 0);
-        unlock_servos();
+    if (strncmp(event->data, "test", event->data_len) == 0) {
+        Serial.printf("test message received");
+        esp_mqtt_client_publish(event->client, MQTT_TOPIC, "test message received", 0, 0, 0);
     }
 
-    if (strncmp(event->data, "lock servos override", event->data_len) == 0) {
-        esp_mqtt_client_publish(event->client, MQTT_TOPIC_MANUAL, "[Control Panel] Overriding servos - LOCK", 0, 0, 0);
-        lock_servos();
+    // Here, if we receive this specific string that will be outputted from control-center, it overrides my local variables turn_on_led
+
+    if (strncmp(event->data, "led-status {on, off} status=on", event->data_len) == 0) {
+        Serial.printf("Received override led-status on");
+        esp_mqtt_client_publish(event->client, MQTT_TOPIC, "Received override led-status on", 0, 0, 0);
+        turn_on_led = true;
     }
 
-
-    if (strncmp(event->data, "unlock servo1 override", event->data_len) == 0) {
-        esp_mqtt_client_publish(event->client, MQTT_TOPIC_MANUAL, "[Control Panel] Overriding servo1 - UNLOCK", 0, 0, 0);
-        unlock_servo(1);
-    }
-    if (strncmp(event->data, "unlock servo2 override", event->data_len) == 0) {
-        esp_mqtt_client_publish(event->client, MQTT_TOPIC_MANUAL, "[Control Panel] Overriding servo2 - UNLOCK", 0, 0, 0);
-        unlock_servo(2);
-    }
-    if (strncmp(event->data, "unlock servo3 override", event->data_len) == 0) {
-        esp_mqtt_client_publish(event->client, MQTT_TOPIC_MANUAL, "[Control Panel] Overriding servo3 - UNLOCK", 0, 0, 0);
-        unlock_servo(3);
-    }
-
-    if (strncmp(event->data, "lock servo1 override", event->data_len) == 0) {
-        esp_mqtt_client_publish(event->client, MQTT_TOPIC_MANUAL, "[Control Panel] Overriding servo1 - LOCK", 0, 0, 0);
-        lock_servo(1);
-    }
-    if (strncmp(event->data, "lock servo2 override", event->data_len) == 0) {
-        esp_mqtt_client_publish(event->client, MQTT_TOPIC_MANUAL, "[Control Panel] Overriding servo2 - LOCK", 0, 0, 0);
-        lock_servo(2);
-    }
-    if (strncmp(event->data, "lock servo3 override", event->data_len) == 0) {
-        esp_mqtt_client_publish(event->client, MQTT_TOPIC_MANUAL, "[Control Panel] Overriding servo3 - LOCK", 0, 0, 0);
-        lock_servo(3);
-    }
-
-    if (strncmp(event->data, "reset control panel", event->data_len) == 0) {
-        esp_mqtt_client_publish(event->client, MQTT_TOPIC_MANUAL, "[Control Panel] Resetting control panel", 0, 0, 0);
-        reset();
+    if (strncmp(event->data, "led-status {on, off} status=off", event->data_len) == 0) {
+        Serial.printf("Received override led-status off");
+        esp_mqtt_client_publish(event->client, MQTT_TOPIC, "Received override led-status off", 0, 0, 0);
+        turn_on_led = false;
     }
 }
 
@@ -171,20 +80,18 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
 unsigned int msg_time = 0;
 esp_mqtt_client_handle_t client = NULL;
 
-
-
 void setup() {
     Serial.begin(115200);
 
-    pinMode(21,INPUT_PULLUP);
-    pinMode(27,INPUT_PULLUP);
-    pinMode(15,INPUT_PULLUP);
+    // CUSTOM CODE FOR YOUR PUZZLE BEGIN: 
 
-    servo1.attach(26);
-    servo2.attach(25);
-    servo3.attach(4); 
-    
+    // For Arduinos, we have to specify which pins are inputs and outputs. Be sure to check the ESP-32 Huzzah Pinout to see which pins can be I/O and which are digital/analog
 
+    pinMode(27,INPUT); // button input monitor
+    pinMode(14,INPUT); // led status monitor
+    pinMode(33, OUTPUT); // led lighter upper
+
+    // CUSTOM CODE END
 
     setup_wifi(WIFI_NAME, WIFI_PASSWORD);
 
@@ -203,82 +110,67 @@ void setup() {
     ESP_ERROR_CHECK(esp_mqtt_client_start(client));
 
     msg_time = millis();
-    
 }
 
 void loop() {
     
+    // CUSTOM CODE FOR YOUR PUZZLE BEGIN: 
+    // This is simply Arduino code. You can look up lots of tutorials for this stuff! 
+
+    led_status = digitalRead(14);
+    button_status = digitalRead(27);
+
+    if (turn_on_led) {
+        digitalWrite(33, HIGH);
+    }
+    else {
+        digitalWrite(33, LOW);
+    }
+
+    // Serial print for debugging
+
+    String s = "digitalRead(14) ";
+    s = s + digitalRead(14);
+    s = s + "      27 ";
+    s = s + digitalRead(27);
+    Serial.println(s);
+
+    // Creation of message for MQTT. The format of an MQTT message must be: 
+    // "variable-name {option1, option2} status=currentstatus"
+    // in order for it to show up correctly on control center
+
+    String message1 = "led-status {on, off} status=";
+    if(led_status) {
+        message1 = message1 + "on";
+    }
+    else {
+        message1 = message1 + "off";
+    }
+
+    String message2 = "button_status {pressed, unpressed} status=";
+    if(button_status) {
+        message2 = message2 + "pressed";
+
+        if (!pressed){
+            turn_on_led = !turn_on_led;
+        }
+        pressed = true;
+    }
+    else {
+        message2 = message2 + "unpressed";
+
+        pressed = false;
+    }
+    
     // put your main code here, to run repeatedly:
-
-    if(digitalRead(21)==0) {
-        button1 = true;
-        servo1.write(unlocked_angle);
-        Serial.println("21");
-    }
-    if(digitalRead(27)==0) {
-        button2 = true;
-        servo2.write(unlocked_angle);
-
-        Serial.println("27");
-
-    }
-    if (digitalRead(15)==0) {
-        button3 = true;
-        Serial.println("15");
-        servo3.write(unlocked_angle);
-
-    }
-
-    if (button1) {
-        servo1.write(unlocked_angle);
-        servo1_status = true;
-    }
-    else {
-        servo1.write(locked_angle);
-    }
-
-    if (button2) {
-        servo2.write(unlocked_angle);
-        servo2_status = true;
-    }
-    else {
-        servo2.write(locked_angle);
-    }
-
-    if (button3) {
-        servo3.write(unlocked_angle);
-        servo3_status = true;
-    }
-    else {
-        servo3.write(locked_angle);
-    }
-
-    if (millis() - msg_time > 1000 && client) {
+    if (millis() - msg_time > 500 && client) { //change the value here to change how often it publishes
         msg_time = millis();
         Serial.println("publishing message");
 
-        String message = "button1: ";
-        message += button1;
-        esp_mqtt_client_publish(client, MQTT_TOPIC, message.c_str(), 0, 0, 0);
-
-        message = "button2: ";
-        message += button2;
-        esp_mqtt_client_publish(client, MQTT_TOPIC, message.c_str(), 0, 0, 0);
-
-        message = "button3: ";
-        message += button3;
-        esp_mqtt_client_publish(client, MQTT_TOPIC, message.c_str(), 0, 0, 0);
-
-        message = "servo1_status: ";
-        message += servo1_status;
-        esp_mqtt_client_publish(client, MQTT_TOPIC, message.c_str(), 0, 0, 0);
-
-        message = "servo2_status: ";
-        message += servo2_status;
-        esp_mqtt_client_publish(client, MQTT_TOPIC, message.c_str(), 0, 0, 0);
-
-        message = "servo3_status: ";
-        message += servo3_status;
-        esp_mqtt_client_publish(client, MQTT_TOPIC, message.c_str(), 0, 0, 0);
+        // Sending our created MQTT strings to the internet. Copy the line below for as many messages as you have. 
+        esp_mqtt_client_publish(client, MQTT_TOPIC, message1.c_str(), 0, 0, 0);
+        esp_mqtt_client_publish(client, MQTT_TOPIC, message2.c_str(), 0, 0, 0);
     }
+
+    // CUSTOM CODE END
 }
