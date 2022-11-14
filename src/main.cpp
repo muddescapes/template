19a54533f7dc9b6b2a6017215a9fa2c06e70daeb
Ashley Cheung <acheung@hmc.edu>
2022-11-13 20:15:50 -0800

    final update for fall22

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ constexpr const char *WIFI_NAME = "Claremont-ETC";
 constexpr const char *WIFI_PASSWORD = "Cl@remontI0T";
 
 // Connect to MQTT
-constexpr const char *MQTT_BROKER = "mqtt://mqtt.eclipseprojects.io";
+constexpr const char *MQTT_BROKER = "mqtt://broker.hivemq.com";
 constexpr const char *MQTT_TOPIC = "muddescapes-esp/test";
 
 // Declare variables: 
@@ -16,7 +16,7 @@ bool led_status = false;
 bool button_status = false;
 
 // This is an internal variable for me to keep track of what should be happening
-bool turn_on_led = false;
+bool led_is_on = false;
 
 // This is an internal variable allowing me to create a "latch" from a button
 bool pressed = false;
@@ -52,22 +52,23 @@ static void mqtt_cb_message(esp_mqtt_event_handle_t event) {
     // and we publish the same message to MQTT (external, public)
 
     if (strncmp(event->data, "test", event->data_len) == 0) {
-        Serial.printf("test message received");
-        esp_mqtt_client_publish(event->client, MQTT_TOPIC, "test message received", 0, 0, 0);
+        const String resp("test message received");
+        Serial.printf(resp.c_str());
+        esp_mqtt_client_enqueue(event->client, MQTT_TOPIC, resp.c_str(), resp.length(), 0, 0, true);
     }
 
     // Here, if we receive this specific string that will be outputted from control-center, it overrides my local variables turn_on_led
 
-    if (strncmp(event->data, "led-status {on, off} status=on", event->data_len) == 0) {
-        Serial.printf("Received override led-status on");
-        esp_mqtt_client_publish(event->client, MQTT_TOPIC, "Received override led-status on", 0, 0, 0);
-        turn_on_led = true;
+    if (strncmp(event->data, "set led-status {on, off} status=on", event->data_len) == 0) {
+        Serial.printf("Received command: led-status on");
+        // esp_mqtt_client_publish(event->client, MQTT_TOPIC, "Received override led-status on", 0, 0, 0);
+        led_is_on = true;
     }
 
-    if (strncmp(event->data, "led-status {on, off} status=off", event->data_len) == 0) {
-        Serial.printf("Received override led-status off");
-        esp_mqtt_client_publish(event->client, MQTT_TOPIC, "Received override led-status off", 0, 0, 0);
-        turn_on_led = false;
+    if (strncmp(event->data, "set led-status {on, off} status=off", event->data_len) == 0) {
+        Serial.printf("Received command: led-status off");
+        // esp_mqtt_client_publish(event->client, MQTT_TOPIC, "Received override led-status off", 0, 0, 0);
+        led_is_on = false;
     }
 }
 
@@ -120,7 +121,7 @@ void loop() {
     led_status = digitalRead(14);
     button_status = digitalRead(27);
 
-    if (turn_on_led) {
+    if (led_is_on) {
         digitalWrite(33, HIGH);
     }
     else {
@@ -129,11 +130,11 @@ void loop() {
 
     // Serial print for debugging
 
-    String s = "digitalRead(14) ";
-    s = s + digitalRead(14);
-    s = s + "      27 ";
-    s = s + digitalRead(27);
-    Serial.println(s);
+    // String s = "digitalRead(14) ";
+    // s = s + digitalRead(14);
+    // s = s + "      27 ";
+    // s = s + digitalRead(27);
+    // Serial.println(s);
 
     // Creation of message for MQTT. The format of an MQTT message must be: 
     // "variable-name {option1, option2} status=currentstatus"
@@ -152,8 +153,9 @@ void loop() {
         message2 = message2 + "pressed";
 
         if (!pressed){
-            turn_on_led = !turn_on_led;
+            led_is_on = !led_is_on; // toggle LED if button was pressed again (after being unpressed)
         }
+        
         pressed = true;
     }
     else {
@@ -168,8 +170,8 @@ void loop() {
         Serial.println("publishing message");
 
         // Sending our created MQTT strings to the internet. Copy the line below for as many messages as you have. 
-        esp_mqtt_client_publish(client, MQTT_TOPIC, message1.c_str(), 0, 0, 0);
-        esp_mqtt_client_publish(client, MQTT_TOPIC, message2.c_str(), 0, 0, 0);
+        esp_mqtt_client_enqueue(client, MQTT_TOPIC, message1.c_str(), message1.length(), 0, 0, true);
+        esp_mqtt_client_enqueue(client, MQTT_TOPIC, message2.c_str(), message2.length(), 0, 0, true);
     }
 
     // CUSTOM CODE END
